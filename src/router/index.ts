8fde235f31ebd2fb8f4149ae16170fa3b0f38e74
webB1an <webB1anyaoyao@gmail.com>
2023-04-07 11:35:54 +0800

    feat(router): modify home router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,17 @@ import type { App } from 'vue'
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '~/layout/index.vue'
-import Nav from '~/pages/navigation/index.vue'
+import Navigation from '~/pages/navigation/index.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    redirect: 'ai-compass',
     component: Layout,
     children: [
       {
-        path: 'ai-compass',
+        path: '',
         name: 'AICompass',
-        component: Nav,
+        component: Navigation,
       },
     ],
   },
